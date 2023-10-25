2dcd58570c2d27e4b5c5a31070aea8858cd09268
X390 <X390@DESKTOP-5MPH0TH>
2023-10-25 18:02:41 +0900

    tempSemi 일부분 가져옴 (완성본 아님)

---
+++ realSemi/src/main/java/test/controller/Test3Controller.java
@@ -0,0 +1,20 @@
+package test.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class Test3Controller extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		request.setAttribute("center", "쌍용강북교육센터");
+		
+	//	super.setRedirect(false);
+		super.setViewPage("/WEB-INF/test/test3.jsp");
+		
+	}
+
+}
---
+++ realSemi/src/main/java/test/controller/Test4Controller.java
@@ -0,0 +1,18 @@
+package test.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class Test4Controller extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		super.setRedirect(true);
+		super.setViewPage(request.getContextPath()+"/test3.up");
+		
+	}
+
+}
