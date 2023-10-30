863f4abeec52b13d83c560c44b13d323b3d91c88
X390 <X390@DESKTOP-5MPH0TH>
2023-10-30 15:59:44 +0900

    ProductDAO, DAO_imple 에 있던 이미지VO 관련된 것 삭제함

--- realSemi/src/main/java/myshop/model/ProductDAO_imple.java
+++ realSemi/src/main/java/myshop/model/ProductDAO_imple.java
@@ -1,14 +1,11 @@
 package myshop.model;
 
 import java.sql.*;
-import java.util.*;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-
-import myshop.domain.ImageVO;
 
 public class ProductDAO_imple implements ProductDAO {
 
@@ -42,39 +39,5 @@ public class ProductDAO_imple implements ProductDAO {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	// 메인페이지에 보여지는 상품이미지파일명을 모두 조회(select)하는 메소드 
-	@Override
-	public List<ImageVO> imageSelectAll() throws SQLException {
-		
-		List<ImageVO> imgList = new ArrayList<>();
-		 
-		try {
-			conn = ds.getConnection();
-			
-			String sql = " select imgno, imgfilename "
-					   + " from tbl_main_image "
-					   + " order by imgno asc ";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				
-				ImageVO imgvo = new ImageVO();
-				imgvo.setImgno(rs.getInt(1));
-				imgvo.setImgfilename(rs.getString(2));
-				
-				imgList.add(imgvo);
-			}// end of while(rs.next())-----------------
-			
-		} finally {
-			close();
-		}
-		
-		return imgList;
-	}// end of public List<ImageVO> imageSelectAll() throws SQLException------
 
 }
